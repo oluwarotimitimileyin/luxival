4dbbb2880d3d10efe1b6112e8be3326062310735
Shopeju Olakunle <sarakuvam@gmail.com>
2026-06-11 23:39:25 +0300

    Add Google-friendly 48px favicon

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "favicon.ico": "favicon.ico" });
   eleventyConfig.addPassthroughCopy({ "favicon-16x16.png": "favicon-16x16.png" });
   eleventyConfig.addPassthroughCopy({ "favicon-32x32.png": "favicon-32x32.png" });
+  eleventyConfig.addPassthroughCopy({ "favicon-48x48.png": "favicon-48x48.png" });
   eleventyConfig.addPassthroughCopy({ "apple-touch-icon.png": "apple-touch-icon.png" });
   eleventyConfig.addPassthroughCopy({ "android-chrome-192x192.png": "android-chrome-192x192.png" });
   eleventyConfig.addPassthroughCopy({ "android-chrome-512x512.png": "android-chrome-512x512.png" });
